bb321362a48b73a498da5a0aeffb874e4ca92e5e
Rupert Connell <rupert@rusrevie.ws>
2021-09-25 17:34:14 +0100

    Update calendars.js

--- calendars.js
+++ calendars.js
@@ -1,8 +1,8 @@
 var cal_1,cal_2,cal_3,cal_4,cal_5,cal_6,cal_7,cal_8,cal_9,cal_10,cal_11,cal_12;     
 window.onload = function () {
-	cal_1  = new Epoch('epoch_popup','popup',document.getElementById('popup_container1'),'2021-11-05','tandem');
-	cal_2  = new Epoch('epoch_popup','popup',document.getElementById('popup_container2'),'2021-11-05','tandem');
-	cal_3  = new Epoch('epoch_popup','popup',document.getElementById('popup_container3'),'2021-11-05','tandem');
+	cal_1  = new Epoch('epoch_popup','popup',document.getElementById('popup_container1'),'2021-11-20','tandem');
+	cal_2  = new Epoch('epoch_popup','popup',document.getElementById('popup_container2'),'2021-11-20','tandem');
+	cal_3  = new Epoch('epoch_popup','popup',document.getElementById('popup_container3'),'2021-11-20','tandem');
 	cal_4  = new Epoch('epoch_popup','popup',document.getElementById('popup_container4'),'2021-12-04','solo');
 	cal_5  = new Epoch('epoch_popup','popup',document.getElementById('popup_container5'),'2021-12-04','solo');
 	cal_6  = new Epoch('epoch_popup','popup',document.getElementById('popup_container6'),'2021-12-04','solo');
