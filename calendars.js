60d4d71d1648f6d9332dc476c8c61a1d05df401f
Rupert Connell <rupert@rusrevie.ws>
2021-09-28 18:23:01 +0100

    Update calendars.js
    Externalise variables

--- calendars.js
+++ calendars.js
@@ -1,6 +1,13 @@
 var cal_1,cal_2,cal_3,cal_4,cal_5,cal_6,cal_7,cal_8,cal_9,cal_10,cal_11,cal_12; 
 var tandem_date = '2021-10-20 00:00:00';
 var solo_date = '2021-12-04 00:00:00';
+var table = document.getElementById("firstdates");
+var rowNode = document.createElement("tr");
+var tanCell = document.createElement("td");
+var soloCell = document.createElement("td");
+var tanNode = document.createTextNode(tandem_date);
+var soloNode = document.createTextNode(solo_date);
+
 window.onload = function () {
 	cal_1  = new Epoch('epoch_popup','popup',document.getElementById('popup_container1'),tandem_date,'tandem');
 	cal_2  = new Epoch('epoch_popup','popup',document.getElementById('popup_container2'),tandem_date,'tandem');
@@ -15,12 +22,6 @@ window.onload = function () {
 	cal_11  = new Epoch('epoch_popup','popup',document.getElementById('popup_container11'),solo_date,'solo');
 	cal_12  = new Epoch('epoch_popup','popup',document.getElementById('popup_container12'),solo_date,'solo');
 
-	var table = document.getElementById("firstdates");
-	var rowNode = document.createElement("tr");
-	var tanCell = document.createElement("td");
-	var soloCell = document.createElement("td");
-	var tanNode = document.createTextNode(tandem_date);
-	var soloNode = document.createTextNode(solo_date);
 	tanCell.appendChild(tanNode);
 	rowNode.appendChild(tanNode);	
 	soloCell.appendChild(soloNode);
