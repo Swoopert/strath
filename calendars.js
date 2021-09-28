d45673494a1d87a90a426492624adc15b8de16d5
Rupert Connell <rupert@rusrevie.ws>
2021-09-28 17:36:12 +0100

    Update calendars.js

--- calendars.js
+++ calendars.js
@@ -1,16 +1,18 @@
-var cal_1,cal_2,cal_3,cal_4,cal_5,cal_6,cal_7,cal_8,cal_9,cal_10,cal_11,cal_12;     
+var cal_1,cal_2,cal_3,cal_4,cal_5,cal_6,cal_7,cal_8,cal_9,cal_10,cal_11,cal_12; 
+var tandem_date = '2021-11-21';
+var solo_date = '2021-12-04';
 window.onload = function () {
-	cal_1  = new Epoch('epoch_popup','popup',document.getElementById('popup_container1'),'2021-11-21','tandem');
-	cal_2  = new Epoch('epoch_popup','popup',document.getElementById('popup_container2'),'2021-11-21','tandem');
-	cal_3  = new Epoch('epoch_popup','popup',document.getElementById('popup_container3'),'2021-11-21','tandem');
-	cal_4  = new Epoch('epoch_popup','popup',document.getElementById('popup_container4'),'2021-12-04','solo');
-	cal_5  = new Epoch('epoch_popup','popup',document.getElementById('popup_container5'),'2021-12-04','solo');
-	cal_6  = new Epoch('epoch_popup','popup',document.getElementById('popup_container6'),'2021-12-04','solo');
-	cal_7  = new Epoch('epoch_popup','popup',document.getElementById('popup_container7'),'2021-12-04','solo');
-	cal_8  = new Epoch('epoch_popup','popup',document.getElementById('popup_container8'),'2021-12-04','solo');
-	cal_9  = new Epoch('epoch_popup','popup',document.getElementById('popup_container9'),'2021-12-04','solo');
-	cal_10  = new Epoch('epoch_popup','popup',document.getElementById('popup_container10'),'2021-12-04','solo');
-	cal_11  = new Epoch('epoch_popup','popup',document.getElementById('popup_container11'),'2021-12-04','solo');
-	cal_12  = new Epoch('epoch_popup','popup',document.getElementById('popup_container12'),'2021-12-04','solo');
+	cal_1  = new Epoch('epoch_popup','popup',document.getElementById('popup_container1'),tandem_date,'tandem');
+	cal_2  = new Epoch('epoch_popup','popup',document.getElementById('popup_container2'),tandem_date,'tandem');
+	cal_3  = new Epoch('epoch_popup','popup',document.getElementById('popup_container3'),tandem_date,'tandem');
+	cal_4  = new Epoch('epoch_popup','popup',document.getElementById('popup_container4'),solo_date,'solo');
+	cal_5  = new Epoch('epoch_popup','popup',document.getElementById('popup_container5'),solo_date,'solo');
+	cal_6  = new Epoch('epoch_popup','popup',document.getElementById('popup_container6'),solo_date,'solo');
+	cal_7  = new Epoch('epoch_popup','popup',document.getElementById('popup_container7'),solo_date,'solo');
+	cal_8  = new Epoch('epoch_popup','popup',document.getElementById('popup_container8'),solo_date,'solo');
+	cal_9  = new Epoch('epoch_popup','popup',document.getElementById('popup_container9'),solo_date,'solo');
+	cal_10  = new Epoch('epoch_popup','popup',document.getElementById('popup_container10'),solo_date,'solo');
+	cal_11  = new Epoch('epoch_popup','popup',document.getElementById('popup_container11'),solo_date,'solo');
+	cal_12  = new Epoch('epoch_popup','popup',document.getElementById('popup_container12'),solo_date,'solo');
 
 };
