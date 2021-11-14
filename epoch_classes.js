1363c6fb3d8bf01a5e06d192dee11eebe33c666c
Rupert Connell <rupert@rusrevie.ws>
2021-11-14 14:05:34 +0000

    Update epoch_classes.js

--- epoch_classes.js
+++ epoch_classes.js
@@ -802,10 +802,10 @@ CalCell.prototype.setClass = function ()  //private: sets the CSS class of the c
 			   	(this.date.getDate() > 9)
 			  ) ||
 		 	  (this.date.getMonth() == 1 && 
-				(this.date.getDate() == 32)
+				(this.date.getDate() != 32)
 			  ) ||
 		 	  (this.date.getMonth() == 2 && 
-				(this.date.getDate() == 32)
+				(this.date.getDate() != 32)
 			  ) ||
 			  (this.date.getMonth() == 3 && 
 				(this.date.getDate() == 3)
@@ -819,7 +819,7 @@ CalCell.prototype.setClass = function ()  //private: sets the CSS class of the c
 		 		(this.date.getDate() == 24)
 			  ) ||
 		 	  (this.date.getMonth() == 11 && 
-				(this.date.getDate() == 1 || this.date.getDate() == 8)
+				((this.date.getDate() == 1) || (this.date.getDate() == 8))
 			  ) ||
 			  this.date >= new Date(2022,0,12)
 			)
